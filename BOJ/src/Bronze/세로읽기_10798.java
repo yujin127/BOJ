0c53b3b415a05e4b293be0b5f323bb1655ba9017
Yujin Oh <yuth9462@gmail.com>
2025-08-02 15:01:48 +0900

    다른 방법으로 푼 세로읽기 문제

--- BOJ/src/Bronze/세로읽기_10798.java
+++ BOJ/src/Bronze/세로읽기_10798.java
@@ -10,27 +10,41 @@ public class 세로읽기_10798 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] tokens = new String[5];
-		for (int i = 0; i < 5; i++) {
-			tokens[i] = br.readLine();
-		}
+//		String[] tokens = new String[5];
+//		for (int i = 0; i < 5; i++) {
+//			tokens[i] = br.readLine();
+//		}
+//		int max = Integer.MIN_VALUE;
+//		char[][] arr = new char[5][];
+//		for (int i = 0; i < 5; i++) {
+//			arr[i] = new char[tokens[i].length()];
+//			if (arr[i].length > max)
+//				max = arr[i].length;
+//			for (int j = 0; j < tokens[i].length(); j++) {
+//				arr[i][j] = tokens[i].charAt(j);
+//			}
+//		}
+//
+//		for (int j = 0; j < max; j++) {
+//			for (int i = 0; i < 5; i++) {
+//				if (j < arr[i].length) {
+//					System.out.print(arr[i][j]);
+//				}
+//			}
+//		}
 		int max = Integer.MIN_VALUE;
-		char[][] arr = new char[5][];
-		for (int i = 0; i < 5; i++) {
-			arr[i] = new char[tokens[i].length()];
-			if (arr[i].length > max)
-				max = arr[i].length;
-			for (int j = 0; j < tokens[i].length(); j++) {
-				arr[i][j] = tokens[i].charAt(j);
+		String[] arr = new String[5];
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<arr.length;i++) {
+			arr[i] = br.readLine();
+			if (max<arr[i].length())max = arr[i].length();
+		}
+		for (int j=0; j<max; j++) {
+			for (int i=0; i<5; i++) {
+				if (i<arr[i].length()) sb.append(arr[i].charAt(j));
 			}
 		}
-
-		for (int j = 0; j < max; j++) {
-			for (int i = 0; i < 5; i++) {
-				if (j < arr[i].length) {
-					System.out.print(arr[i][j]);
-				}
-			}
-		}
+		System.out.println(sb);
+		
 	}
 }
