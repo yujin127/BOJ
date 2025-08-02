04b68abde2cb38a74021a788d5d7203c794b15f8
Yujin Oh <yuth9462@gmail.com>
2025-08-02 14:34:05 +0900

    intial commit

---
+++ BOJ/src/Bronze/세로읽기_10798.java
@@ -0,0 +1,36 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/10798
+
+public class 세로읽기_10798 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] tokens = new String[5];
+		for (int i = 0; i < 5; i++) {
+			tokens[i] = br.readLine();
+		}
+		int max = Integer.MIN_VALUE;
+		char[][] arr = new char[5][];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = new char[tokens[i].length()];
+			if (arr[i].length > max)
+				max = arr[i].length;
+			for (int j = 0; j < tokens[i].length(); j++) {
+				arr[i][j] = tokens[i].charAt(j);
+			}
+		}
+
+		for (int j = 0; j < max; j++) {
+			for (int i = 0; i < 5; i++) {
+				if (j < arr[i].length) {
+					System.out.print(arr[i][j]);
+				}
+			}
+		}
+	}
+}
