c002da9e8df3b2bedf121ace771c1047d4ccfd2f
Yujin Oh <yuth9462@gmail.com>
2025-08-08 00:52:48 +0900

    0807

--- BOJ/src/bronze/알고리즘수업_버블정렬1_23968.java
+++ BOJ/src/bronze/알고리즘수업_버블정렬1_23968.java
@@ -11,5 +11,28 @@ public class 알고리즘수업_버블정렬1_23968 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int count = 0;
+		for (int i=n-1; i>0; i--) {
+			for (int j=0; j<i; j++) {
+				if (arr[j]>arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					count ++;
+
+					if (count == k) {
+						System.out.println(Math.min(arr[j], arr[j+1])+ " "+Math.max(arr[j], arr[j+1]));
+						return;
+					}
+				}
+			}
+		}
+		System.out.println(-1);
 	}
 }
