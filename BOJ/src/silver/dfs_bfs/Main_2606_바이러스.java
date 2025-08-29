c353c73ed96f1b01d81fe586b37acbe255d380be
Yujin <yuth9462@gmail.com>
2025-08-29 17:41:54 +0900

    boj virus

---
+++ BOJ/src/silver/dfs_bfs/Main_2606_바이러스.java
@@ -0,0 +1,49 @@
+package silver.dfs_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_2606_바이러스 {
+	static int n, m;
+	static boolean[] visited;
+	static int[][] arr;
+	static int cnt = 0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		arr = new int[n+1][n+1];
+		visited = new boolean[n+1];
+		for (int i=0; i<m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a][b] = arr[b][a] = 1;
+		}	
+		System.out.println(bfs(1));
+	}
+	
+	public static int bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		visited[start] = true;
+		q.add(start);
+		
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			for (int i=1; i<=n; i++) {
+				if (arr[cur][i] == 1 && !visited[i]) {
+					visited[i] = true;
+					q.add(i);
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+   	}
+}
