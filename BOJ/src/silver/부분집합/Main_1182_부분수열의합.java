ad17220a857283a5975e18c8fb92e54b18d63e57
Yujin Oh <yuth9462@gmail.com>
2025-09-04 00:58:42 +0900

    powerset review

---
+++ BOJ/src/silver/부분집합/Main_1182_부분수열의합.java
@@ -0,0 +1,44 @@
+package silver.부분집합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1182_부분수열의합 {
+	static int n, s, ans;
+	static int[] arr;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		visited = new boolean[n];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		ans = 0;
+		powerset(0);
+		System.out.println(ans);
+	}
+	public static void powerset(int idx) {
+		if (idx == n) {
+			int tmp = 0;
+			boolean picked = false;
+			for (int i=0; i<n; i++) {
+				if (visited[i]) {
+					tmp += arr[i];
+					picked = true;
+				}
+			}
+			if (picked && tmp==s) ans ++;
+			return;
+		}
+		visited[idx] = true;
+		powerset(idx + 1);
+		visited[idx] = false;
+		powerset(idx + 1);
+	}
+}
