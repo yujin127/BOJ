98d2726cb6b084cf6d387cf9633f41ad78889c37
yujin127 <yuth9462@gmail.com>
2025-09-05 17:34:59 +0900

    0905 aps

--- BOJ/src/Permutation/Main_9663_NQueen.java
+++ BOJ/src/Permutation/Main_9663_NQueen.java
@@ -3,16 +3,34 @@ package Permutation;
 import java.util.Scanner;
 
 public class Main_9663_NQueen {
-	static int n;
+	static int n, ans;
 	static int[] chess;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		chess = new int[n];
+		ans = 0;
 		queen(0);
+		System.out.println(ans);
 	}
 	
-	static void queen(int idx) {
-		if (idx =)
+	static void queen(int row) {
+		if (row == n) {
+			ans ++;
+			return;
+		}
+		for (int col=0; col<n; col++) {
+			if (safe(row, col)) {
+				chess[row] = col;  
+				queen(row+1);
+			}
+		}
+	}
+	static boolean safe(int row, int col) {
+		for (int i=0; i<row; i++) {
+			if (col==chess[i]) return false;
+			if ((row-i) == Math.abs(chess[i]-col)) return false;
+		}
+		return true;
 	}
 }
